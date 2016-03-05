bb7fbd4835b165a7b68a4f4f92d22a1f298fc87f
Roberto Pesce <robertopescee@hotmail.com>
2016-03-05 21:17:17 +0000

    lecture 66

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -16,6 +20,16 @@ class ArticlesController < ApplicationController
 
 def show
   @article = Article.find(params[:id])
+end
+
+def update
+  @article = Article.find(params[:id])
+  if @article.update(article_params)
+    flash[:notice] = "Your article was updated"
+    redirect_to article_path(@article)
+  else
+    render 'edit'
+  end
 end
   
   private

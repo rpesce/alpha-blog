18a26af423fbf7da08b186cb5be553bcf319009c
Roberto Pesce <robertopescee@hotmail.com>
2016-03-06 19:03:22 +0000

    Lecture 70

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def index
+    @articles = Article.all
+  end
+  
   def edit
     @article = Article.find(params[:id])
   end
@@ -30,6 +34,13 @@ def update
   else
     render 'edit'
   end
+end
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+  flash[:notice] = "Article deleted"
+  redirect_to articles_path
 end
   
   private

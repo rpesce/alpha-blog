53a35c08a13193f575369ea4bd76add69d7f2618
Roberto Pesce <robertopescee@hotmail.com>
2016-03-17 23:20:38 +0000

    lecture 103

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,22 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
-    
   end
+    
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account has been updated"
+      redirect_to articles_path
+    else
+      render 'edit'  
+    end
+  end
+  
   
   private
   def user_params

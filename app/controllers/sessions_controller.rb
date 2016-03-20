346875b1a08b7d95f4d7b1c764b5ca56e280adb7
Roberto Pesce <robertopescee@hotmail.com>
2016-03-20 19:50:52 +0000

    lecture 117

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,21 @@ class SessionsController < ApplicationController
   end
   
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have logged in!"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "There is something wrong with the information you have provided"
+      render 'new'
+    end
+    
   end
   
   def destroy
-    
+    session[:user_id] = nil
+    flash[:success] = "You have logged out!"
+    redirect_to root_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+before_action :set_user, only: [:edit, :update, :show]
+
 def index
   @users = User.paginate(page: params[:page], per_page: 5)
 end
@@ -19,11 +21,9 @@ end
   end
     
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account has been updated"
       redirect_to articles_path
@@ -33,7 +33,6 @@ end
   end
   
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
@@ -42,5 +41,7 @@ end
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-  
+  def set_user
+    @user = User.find(params[:id])
+  end
 end

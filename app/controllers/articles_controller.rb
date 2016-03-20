1d1c23863683bd49c2bf1f7088ef1a8ffd90c545
Roberto Pesce <robertopescee@hotmail.com>
2016-03-20 20:42:51 +0000

    lecture 119

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :show, :update, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def new
     @article = Article.new
@@ -14,7 +16,7 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "The article was created"
       redirect_to article_path(@article)  
@@ -49,6 +51,13 @@ end
   
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+  
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "You cannot do it!"
+      redirect_to root_path
+    end
   end
   
 end

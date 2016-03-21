dc64a0d87507c6692d6ba0ee237978c011ed3455
Roberto Pesce <robertopescee@hotmail.com>
2016-03-21 20:44:41 +0000

    lesson 121

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,9 @@ end
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome, #{@user.username}, to the Alpha Blog"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end

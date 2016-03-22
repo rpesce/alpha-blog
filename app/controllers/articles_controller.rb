b2db56d8a849284c77c70120d289f76f782671b8
Roberto Pesce <robertopescee@hotmail.com>
2016-03-22 21:25:35 +0000

    lesson 123

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -54,7 +54,7 @@ end
   end
   
   def require_same_user
-    if current_user != @article.user
+    if current_user != @article.user and !current_user.admin?
       flash[:danger] = "You cannot do it!"
       redirect_to root_path
     end
